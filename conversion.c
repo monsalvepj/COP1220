224a9cd64969416ad5258740068716e8aacf0ce2
Pedro <monsalvepj@my.palmbeachstate.edu>
2015-08-03 04:08:34 -0400

    setup

---
+++ conversion.c
@@ -0,0 +1,40 @@
+/*2.) CONVERSION: TEMPERATURE
+ Implement the following integer functions:
+a)	Function celsius returns the Celsius equivalent of a Fahrenheit temperature.
+b)	Function fahrenheit returns the Fahrenheit equivalent of a Celsius temperature.
+c)	Use these functions to write a program that prints charts showing the Fahrenheit equivalents of all Celsius temperatures from 0 to 100 degrees,
+and the Celsius equivalents of all Fahrenheit temperatures from 32 to 212 degrees. Print the outputs in a neat tabular format that minimizes
+the number of lines of output while remaining readable.*/
+
+
+//Pedro Monsalve - P07496482 - Temperature Conversion
+#include <stdio.h>
+
+int fahrenheitFunc (int x);
+
+int celciusFunc (int x);
+
+int main(){
+	
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+int fahrenheitFunc (int x){
+	
+}
+
+
+
+
+
+
+int celciusFunc (int x){
+	
+}
